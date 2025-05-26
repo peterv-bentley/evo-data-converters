321677e97e8bd869f4d4ca0c07c6ca817225edee
Arthur Gordon-Wright <26261951+ArthurGW@users.noreply.github.com>
2025-05-26 17:25:57 +1200

    Ruff changes

--- packages/duf/src/evo/data_converters/duf/__init__.py
+++ packages/duf/src/evo/data_converters/duf/__init__.py
@@ -2,4 +2,4 @@ from .common import ObjectCollector, Polyface, Polyline
 from .duf_reader_context import DufCollectorContext
 from .utils import is_duf
 
-__all__ = ["is_duf", "DufCollectorContext", "ObjectCollector"]
+__all__ = ["is_duf", "DufCollectorContext", "ObjectCollector", "Polyface", "Polyline"]
--- packages/duf/tests/importer/test_duf_attribute_to_evo.py
+++ packages/duf/tests/importer/test_duf_attribute_to_evo.py
@@ -9,7 +9,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-import tempfile
 from datetime import datetime, timezone
 from os import path
 from typing import Any, TypeAlias
