b58bab611b07849c74b9266f966a9997b5186912
Peter Varg <231590768+peterv-bentley@users.noreply.github.com>
2025-10-23 13:28:02 +1300

    Add documentation
    This adds some python docstrings to some classes.

--- packages/common/src/evo/data_converters/common/grid_data.py
+++ packages/common/src/evo/data_converters/common/grid_data.py
@@ -17,6 +17,20 @@ import numpy.typing as npt
 
 @dataclass
 class BaseGridData:
+    """
+    Base representation of a 3D grid
+
+    Attributes:
+    -----------
+    origin: list[float] - The coordinates of the origin [x,y,z]
+    size: list[int] - The size of the entire grid [grid_size_x, grid_size_y, grid_size_z]
+    rotation: numpy NDArray[numpy float] - Orientation of the grid [dip_azimuth, dip, pitch]
+    bounding_box: list[float] | None - Bounding box of the spatial data
+    mask: numpy NDArray[numpy bool] | None - Indicates which cells have values
+    cell_attributes: dict[str, numpy NDArray] | None - Attributes associated with the cells
+    vertex_attributes: dict[str, numpy NDArray] | None - Attributes associated with the vertices
+    """
+
     origin: list[float]
     size: list[int]
     rotation: npt.NDArray[np.float_]
@@ -28,11 +42,43 @@ class BaseGridData:
 
 @dataclass
 class RegularGridData(BaseGridData):
+    """
+    Representation of a regular 3D grid
+
+    Attributes:
+    -----------
+    origin: list[float] - The coordinates of the origin [x,y,z]
+    size: list[int] - The size of the entire grid [grid_size_x, grid_size_y, grid_size_z]
+    rotation: numpy NDArray[numpy float] - Orientation of the grid [dip_azimuth, dip, pitch]
+    bounding_box: list[float] | None - Bounding box of the spatial data
+    mask: numpy NDArray[numpy bool] | None - Indicates which cells have values
+    cell_attributes: dict[str, numpy NDArray] | None - Attributes associated with the cells
+    vertex_attributes: dict[str, numpy NDArray] | None - Attributes associated with the vertices
+    cell_size: list[float] - The size of each cell in the grid [cell_size_x, cell_size_y, cell_size_z]
+    """
+
     cell_size: list[float]
 
 
 @dataclass
 class TensorGridData(BaseGridData):
+    """
+    Representation of a tensor 3D grid, where cells may have different sizes
+
+    Attributes:
+    -----------
+    origin: list[float] - The coordinates of the origin [x,y,z]
+    size: list[int] - The size of the entire grid [grid_size_x, grid_size_y, grid_size_z]
+    rotation: numpy NDArray[numpy float] - Orientation of the grid [dip_azimuth, dip, pitch]
+    bounding_box: list[float] | None - Bounding box of the spatial data
+    mask: numpy NDArray[numpy bool] | None - Indicates which cells have values
+    cell_attributes: dict[str, numpy NDArray] | None - Attributes associated with the cells
+    vertex_attributes: dict[str, numpy NDArray] | None - Attributes associated with the vertices
+    cell_sizes_x: list[float] - Grid cell sizes along the x axis
+    cell_sizes_y: list[float] - Grid cell sizes along the y axis
+    cell_sizes_z: list[float] - Grid cell sizes along the z axis
+    """
+
     cell_sizes_x: list[float]
     cell_sizes_y: list[float]
     cell_sizes_z: list[float]
